b83739715a3c46f000447e996ca75aeae14a2bdf
Wasim <imwasim.shaik@gmail.com>
2026-01-15 20:35:26 +0530

    tfvars for multiple env

---
+++ tfvars/dev/backend.tf
@@ -0,0 +1,5 @@
+ bucket       = "remote-state-wasdaws-dev"
+ key          = "multi-env-dev"
+ region       = "us-east-1"
+ use_lockfile = true
+ encrypt      = true
---
+++ tfvars/prod/backend.tf
@@ -0,0 +1,5 @@
+ bucket       = "remote-state-wasdaws-prod"
+ key          = "multi-env-prod"
+ region       = "us-east-1"
+ use_lockfile = true
+ encrypt      = true
