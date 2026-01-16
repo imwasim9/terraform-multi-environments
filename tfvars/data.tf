1496e4a9d442373fdf5ddeeb698f0e89e38ab54d
Wasim <imwasim.shaik@gmail.com>
2026-01-16 06:31:47 +0530

    terraform-multi-env

--- tfvars/data.tf
+++ tfvars/data.tf
@@ -1,11 +1,28 @@
-data "aws_ami" "joindevops"{
-    owners=["973714476881"]
-    most_recent = true
+# data "aws_ami" "joindevops"{
+#     owners=["973714476881"]
+#     most_recent = true
+
+#     filter {
+#         name = "name" # Name is case sensitive here. should use only name as value
+#         values = ["RHEL-9-DevOps-Practice"]
+#     }
+# }
+
+data "aws_ami" "joindevops" {
+    owners = ["973714476881"]
+    # most_recent = true
+
 
     filter {
-        name = "name" # Name is case sensitive here. should use only name as value
-        values = ["RHEL-9-DevOps-Practice"]
+        name = "name"
+        values = ["Redhat-9-DevOps-Practice"]
     }
+
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+
 }
 
 output "ami_id" {
