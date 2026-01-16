5d53d6490ea2a53e8fae7a6467c7d09a5e91d4bc
Wasim <imwasim.shaik@gmail.com>
2026-01-17 05:27:51 +0530

    terraform message

--- workspace/provider.tf
+++ workspace/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket       = "remote-state-wasdaws"
+    bucket       = "remotestate-wasdaws"
     key          = "multi-env-workspace"
     region       = "us-east-1"
     use_lockfile = true
